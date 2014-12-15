9833e83395bef344d0242dfaaf6757f5de9d10d3
Alex Vandiver <alexmv@bestpractical.com>
2014-12-15 16:56:48 -0500

    Version 1.00 releng

--- lib/RT/BugTracker/Public.pm
+++ lib/RT/BugTracker/Public.pm
@@ -53,7 +53,7 @@ use warnings;
 package RT::BugTracker::Public;
 use URI::Escape qw/ uri_escape /;
 
-our $VERSION = '0.05';
+our $VERSION = '1.00';
 
 RT->AddJavaScript("bugtracker-public.js");
 RT->AddStyleSheets("bugtracker-public.css");
@@ -61,6 +61,34 @@ RT->AddStyleSheets("bugtracker-public.css");
 =head1 NAME
 
 RT::BugTracker::Public - Adds a public, (hopefully) userfriendly bug tracking UI to RT
+
+=head1 INSTALLATION
+
+=over
+
+=item C<perl Makefile.PL>
+
+=item C<make>
+
+=item C<make install>
+
+May need root permissions
+
+=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+
+Add this line:
+
+    Set(@Plugins, qw(RT::BugTracker::Public));
+
+or add C<RT::BugTracker::Public> to your existing C<@Plugins> line.
+
+=item Clear your mason cache
+
+    rm -rf /opt/rt4/var/mason_data/obj
+
+=item Restart your webserver
+
+=back
 
 =head1 CONFIGURATION
 
@@ -139,11 +167,25 @@ sub RedirectToPublic {
 
 =head1 AUTHOR
 
-Thomas Sibley E<lt>trs@bestpractical.comE<gt>
+Best Practical Solutions, LLC E<lt>modules@bestpractical.comE<gt>
 
-=head1 LICENSE
+=head1 BUGS
 
-GPL version 2.
+All bugs should be reported via email to
+
+    L<bug-RT-BugTracker-Public@rt.cpan.org|mailto:bug-RT-BugTracker-Public@rt.cpan.org>
+
+or via the web at
+
+    L<rt.cpan.org|http://rt.cpan.org/Public/Dist/Display.html?Name=RT-BugTracker-Public>.
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2014 by Best Practical Solutions
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 2, June 1991
 
 =cut
 
