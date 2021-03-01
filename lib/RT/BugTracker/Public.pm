b72305dcf5c66cb3da9254bd3e224b7449682157
sunnavy <sunnavy@bestpractical.com>
2021-03-02 04:37:32 +0800

    Update doc for rt4 => rt5

--- lib/RT/BugTracker/Public.pm
+++ lib/RT/BugTracker/Public.pm
@@ -102,7 +102,7 @@ May need root permissions
 
 =item C<make initdb>
 
-=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+=item Edit your F</opt/rt5/etc/RT_SiteConfig.pm>
 
 Add this line:
 
@@ -110,7 +110,7 @@ Add this line:
 
 =item Clear your mason cache
 
-    rm -rf /opt/rt4/var/mason_data/obj
+    rm -rf /opt/rt5/var/mason_data/obj
 
 =item Restart your webserver
 
